904b769fa623bb94314018643154cdb7437c8c7a
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2014-01-25 14:23:56 +0100

    small code style fixes

--- Module.php
+++ Module.php
@@ -1,14 +1,13 @@
 <?php
 namespace PHPConfig;
 
-
 /**
  * Allow to set php.ini values on the fly.
  */
 class Module
 {
     /**
-     * @param Zend\Mvc\MvcEvent $mvcEvent
+     * @param \Zend\Mvc\MvcEvent $mvcEvent
      */
     public function onBootstrap($mvcEvent)
     {
@@ -18,9 +17,7 @@ class Module
         /** @var $config array */
         $config = $application->getConfig();
 
-        if (array_key_exists('PHPConfig', $config)
-            && is_array($config['PHPConfig'])
-        ) {
+        if (array_key_exists('PHPConfig', $config) && is_array($config['PHPConfig'])) {
             foreach ($config['PHPConfig'] as $key => $value) {
                 ini_set($key, $value);
             }
